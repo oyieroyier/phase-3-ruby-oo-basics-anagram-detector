6f3bad6ee715f7165c21a83af98a228fd0933444
Bob Oyier <boboyier92@gmail.com>
2023-02-13 11:35:45 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_reader :initial_word
+
+  def initialize(initial_word)
+    @initial_word = initial_word
+  end
+
+  def match(arr)
+    arr.filter { |word| word.chars.sort == @initial_word.chars.sort }
+  end
+end
